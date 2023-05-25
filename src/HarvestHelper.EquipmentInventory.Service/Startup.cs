c23b994d6269096b07e8c398b03c27a919cab370
Duarte Somsen <ds@letsgrow.com>
2023-05-26 01:15:45 +0200

    Fixed CORS errors

--- src/HarvestHelper.EquipmentInventory.Service/Startup.cs
+++ src/HarvestHelper.EquipmentInventory.Service/Startup.cs
@@ -22,6 +22,8 @@ namespace HarvestHelper.EquipmentInventory.Service
 {
     public class Startup
     {
+        private const string AllowedOriginSetting = "AllowedOrigin";
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -56,6 +58,13 @@ namespace HarvestHelper.EquipmentInventory.Service
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HarvestHelper.EquipmentInventory.Service v1"));
+
+                app.UseCors(builder =>
+                {
+                    builder.WithOrigins(Configuration[AllowedOriginSetting])
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });  
             }
 
             app.UseHttpsRedirection();
